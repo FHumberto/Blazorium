63661bdd7aa16ddb99eff2d789f5be02fdbdcca6
FHumberto <7258900+FHumberto@users.noreply.github.com>
2025-06-28 20:48:28 +0000

    Deploying to gh-pages from  @ 223bd132132a19e97cb5639ab9d848880448cf80 🚀

---
+++ js/PointerLockControls.js
@@ -0,0 +1,15 @@
+﻿let camera, scene, renderer;
+let moveForward = false, moveBackward = false, moveLeft = false, moveRight = false;
+let velocity = { x: 0, z: 0 };
+let direction = { x: 0, z: 0 };
+let yaw = 0, pitch = 0;
+
+window.ativarControlesDoomLike = () => {
+    const canvas = document.querySelector("canvas");
+    if (!canvas) {
+        console.warn("Canvas não encontrado.");
+        return;
+    }
+
+    //! TOODO: PEGAR CÂMERA E CENA DO JOGO
+}
